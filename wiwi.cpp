7612578f7f795d823154ebfba46b810c4e9d3428
Adrian649 <adrian.irizarry@upr.edu>
2021-02-05 09:07:47 -0400

    Practica sisisis

---
+++ wiwi.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Mucha practica sisisis" << endl;
+    int n = 0;
+    while (n == 0) {
+        cout << "WIIIIWIIIIIWIIIIWIIIIIWIIIIIIWIIIIII" << endl;
+    }
+    return 0;
+}
